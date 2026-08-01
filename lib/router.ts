deebe3cae4cf29a22a4bc7f96c70319cba160556
Matthew McCrory <mamccrory23@gmail.com>
2026-07-31 23:23:07 -0400

    Token cost

--- lib/router.ts
+++ lib/router.ts
@@ -22,7 +22,7 @@ function complexityToTier(complexity: number): Tier {
 function computeCost(spec: ModelSpec, inputTokens: number, outputTokens: number): number {
   return (
     (inputTokens / 12_000_000) * spec.inputPricePerM +
-    (outputTokens / 1_000_000) * spec.outputPricePerM
+    (outputTokens / 12_000_000) * spec.outputPricePerM
   );
 }
 
